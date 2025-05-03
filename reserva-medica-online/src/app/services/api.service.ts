7876c68a567ad50565d362ae3fda8ea8b2ff8500
jonjonathanarias <106196489+jonjonathanarias@users.noreply.github.com>
2025-05-03 20:18:51 -0300

    acomodando url api

--- reserva-medica-online/src/app/services/api.service.ts
+++ reserva-medica-online/src/app/services/api.service.ts
@@ -9,20 +9,20 @@ import { TurnosComponent } from "../pages/turnos/turnos.component";
 export class ApiService {
     //private apiUrl = 'http://192.168.0.170:8000';
     //  private apiUrl = 'http://localhost:8000/';
-     private apiUrl = 'http://127.0.0.1:8000';
+     //private apiUrl = 'http://127.0.0.1:8000';
     // private apiUrl = 'http://mgalarmasserver1.ddns.net:8000/';
-     //private apiUrl = 'http://casaiot.ddns.net:8000/';
+     private apiUrl = 'https://reservasmedicas.ddns.net/';
 
     constructor(private http: HttpClient ) { }
 
     register(user: any):
         Observable<any> {
-        return this.http.post<any>(this.apiUrl + '/register/', user);
+        return this.http.post<any>(this.apiUrl + 'register/', user);
     }
 
     login(dni: string, password: string): Observable<any> {
         const body = { username: dni, password: password };
-        return this.http.post<any>(this.apiUrl + '/login/', body).pipe(
+        return this.http.post<any>(this.apiUrl + 'login/', body).pipe(
         );
     }
 
