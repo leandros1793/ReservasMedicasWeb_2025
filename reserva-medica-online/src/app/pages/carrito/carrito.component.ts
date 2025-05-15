f3d5d264686ab674824140f971b3b2db68a38aff
leandros1793 <leandro.s.cba@gmail.com>
2025-05-14 23:06:24 -0300

    actualizacion del componente carrito y carrito service

--- reserva-medica-online/src/app/pages/carrito/carrito.component.ts
+++ reserva-medica-online/src/app/pages/carrito/carrito.component.ts
@@ -1,59 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
-import { Especialidad } from '../../interfaces/especialidad';
-import { CarritoService } from '../../services/carrito.service';
 import { CommonModule } from '@angular/common';
-import { ApiService } from '../../services/api.service';
+import { CarritoService } from '../../services/carrito.service';
 
 @Component({
   selector: 'app-carrito',
   standalone: true,
-  imports: [FormsModule, CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './carrito.component.html',
   styleUrls: ['./carrito.component.css']
 })
 export class CarritoComponent implements OnInit {
-  carritoVacio: boolean = false;
-  usuarioLogueado: boolean = true;
-  especialidad: Especialidad | undefined;
 
-  carritoItems: any[] = [];
-  total: number = 0;
+  turnos: any[] = [];
 
-  constructor(private router: Router, private carritoService: CarritoService, private apiService: ApiService) {}
+  constructor(
+    private carritoService: CarritoService,
+    private router: Router
+  ) {}
 
-  ngOnInit() {
-    if (this.apiService.isLoggedIn()){
-      this.router.navigate(['/carrito']);
-    } else {
-      alert('Por favor, Inicia sesión para agendar un turno');
-      this.router.navigate(['/iniciarSesion']);
-    }
-    this.carritoItems = this.carritoService.obtenerCarritoItems();
-    this.actualizarTotal();
+  ngOnInit(): void {
+    this.cargarTurnos();
   }
 
-  actualizarFechaHorario(index: number, event: Event, tipo: string) {
-    const valor = (event.target as HTMLInputElement).value;
-    if (tipo === 'fecha') {
-      this.carritoItems[index].fecha = valor;
-    } else if (tipo === 'horario') {
-      this.carritoItems[index].horario = valor;
-    }
-    this.carritoService.actualizarItem(index, this.carritoItems[index].fecha, this.carritoItems[index].horario);
-    this.actualizarTotal();
+  cargarTurnos(): void {
+    this.turnos = this.carritoService.obtenerTurnos();
   }
 
-  actualizarTotal() {
-    this.total = this.calcularTotal();
+  irATurnos(): void {
+    this.router.navigate(['/turnos']);
   }
 
-  calcularTotal() {
-    return this.carritoItems.reduce((total, item) => total + (item.precio * item.cantidad), 0);
+  eliminarTurno(index: number): void {
+    this.carritoService.eliminarTurno(index);
+    this.cargarTurnos(); // Recarga después de eliminar
   }
 
-  irAPasarelaPagos() {
-    this.router.navigate(['/pasarela-pagos']);
+  limpiarCarrito(): void {
+    this.carritoService.limpiarCarrito();
+    this.cargarTurnos();
   }
 }
--- reserva-medica-online/src/app/services/carrito.service.ts
+++ reserva-medica-online/src/app/services/carrito.service.ts
@@ -1,56 +1,41 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarritoService {
 
-  private carritoItems: any[] = [];
-  private carritoItemCount = new BehaviorSubject<number>(0);
-  carritoItemCount$ = this.carritoItemCount.asObservable();
+  private turnos: any[] = [];
 
-  Carrito: any;
-
-  constructor() { }
-
-  agregarAlCarrito(item: any) {
-
-    const existingItem = this.carritoItems.find(i => i.id === item.id);
-    if (existingItem) {
-      existingItem.cantidad++;
-      existingItem.subtotal += item.precio;
-    } else {
-      const carritoItem = {
-        id: item.id,
-        titulo: item.titulo,
-        precio: item.precio,
-        cantidad: 1,
-        fecha: '',
-        horario: '',
-        subtotal: item.precio
-      };
-      this.carritoItems.push(carritoItem);
-    }
-    this.carritoItemCount.next(this.carritoItems.reduce((acc, item) => acc + item.cantidad, 0));
-  }
-  
-  
-
-  obtenerCarritoItems() {
-    return this.carritoItems;
-  }
-
-  actualizarItem(index: number, fecha: string, horario: string) {
-    if (this.carritoItems[index]) {
-      this.carritoItems[index].fecha = fecha;
-      this.carritoItems[index].horario = horario;
+  constructor() {
+    const data = localStorage.getItem('carritoTurnos');
+    if (data) {
+      this.turnos = JSON.parse(data);
     }
   }
 
-  obtenerTotal() {
-    return this.carritoItems.reduce((total, item) => total + item.subtotal, 0);
+  agregarTurno(turno: any) {
+    this.turnos.push(turno);
+    this.guardarEnLocalStorage();
   }
 
+  obtenerTurnos(): any[] {
+    return this.turnos;
+  }
 
+  eliminarTurno(index: number) {
+    if (index >= 0 && index < this.turnos.length) {
+      this.turnos.splice(index, 1);
+      this.guardarEnLocalStorage();
+    }
+  }
+
+  limpiarCarrito() {
+    this.turnos = [];
+    this.guardarEnLocalStorage();
+  }
+
+  private guardarEnLocalStorage() {
+    localStorage.setItem('carritoTurnos', JSON.stringify(this.turnos));
+  }
 }
